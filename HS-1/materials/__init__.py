2cd401732782e85ab8369c7d68e9fd29e24f322c
Hugo Maltese <hugo.maltese@epitech.eu>
2025-12-12 00:19:40 +0100

    rm hs1

--- HS-1/materials/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Material database for fusion reactor components."""
-
-from .materials import Material, MaterialDatabase
-
-__all__ = ['Material', 'MaterialDatabase']
-
--- HS-1/visualization/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Visualization tools for fusion reactor simulator."""
-
-from .plotter import ReactorPlotter
-
-__all__ = ['ReactorPlotter']
-
